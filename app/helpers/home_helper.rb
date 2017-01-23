b9a34dbd5249d3ce03305e14b4b480efd1dc0a44
Odialko <viktor1532@gmail.com>
2017-01-23 15:15:35 +0200

    add TODO:

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,7 @@
 module HomeHelper
+
+  # TODO: хедпер для css
+  # def add_css_class(param)
+  #   (params[:page].eql? param) ? 'main' : ''
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'main', to: 'home#main', page: 'main'
+  # TODO: Роути з необов*язковий параметер і аліас
+  get 'main', to: 'home#main', page: 'main', as: 'main_page'
 
   root 'home#index'
 end
