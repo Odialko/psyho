4db3d27811075eefe14a6a6ae0cd260e0401e11e
Andrii Nizinkovskyi <enemy.vin@gmail.com>
2017-01-31 13:40:10 +0000

    Update home_helper.rb

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -7,7 +7,8 @@ module HomeHelper
 
   def inline_svg(path)
     file_path = "#{Rails.root}/app/assets/images/#{path}"
-    return File.read(file_path).html_safe if File.exist?(file_path) else ""
+    return File.read(file_path).html_safe if File.exist?(file_path)
+    ''
   end
 
 end
